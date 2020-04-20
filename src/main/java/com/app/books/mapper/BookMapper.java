499c5f2a5ff7dfa1002111b8b5f04355fb812491
william <jackson20199988@gmail.com>
2020-04-20 17:54:59 +0400

    修复单部小说详情bug

--- src/main/java/com/app/books/mapper/BookMapper.java
+++ src/main/java/com/app/books/mapper/BookMapper.java
@@ -71,7 +71,7 @@ public interface BookMapper {
      * @param bookId
      * @return
      */
-    @Select("select c.comment_info as commentInfo \n" +
+    @Select("select c.comment_info as commentInfo, \n" +
             "(select t_user.user_name from t_user where t_user.id = c.user_id) as userName \n" +
             "from t_comment c where c.out_id = #{bookId}\n" +
             "ORDER BY c.create_time desc")
