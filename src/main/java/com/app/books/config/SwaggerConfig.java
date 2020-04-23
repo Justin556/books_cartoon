fd20b32a88d084b2762c3c99ad1fa9f0eab2db3a
william <jackson20199988@gmail.com>
2020-04-23 04:01:45 +0400

    配置swagger

--- src/main/java/com/app/books/config/SwaggerConfig.java
+++ src/main/java/com/app/books/config/SwaggerConfig.java
@@ -1,42 +1,65 @@
 package com.app.books.config;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig{
-        @Bean
-        public Docket api() {
-            return new Docket(DocumentationType.SWAGGER_2)
-                    .apiInfo(apiInfo())
-                    .pathMapping("/")
-                    // 选择那些路径和api会生成document
-                    .select()
-                    // 对所有api进行监控
-                    .apis(RequestHandlerSelectors.any())
-                    //不显示错误的接口地址
-                    //错误路径不监控
-                    .paths(Predicates.not(PathSelectors.regex("/error.*")))
-                    // 对根下所有路径进行监控
-                    .paths(PathSelectors.regex("/.*"))
-                    .build();
-        }
+public class SwaggerConfig {
 
-        private ApiInfo apiInfo() {
-            return new ApiInfoBuilder().title("springboot_shop接口文档")
-                    .contact(new Contact("wxySmile", "http://wxy97.com/", "wxyrrcj@163.com"))
-                    .description("个人博客地址：http://wxy97.com/")
-                    .version("v1.0")
-                    .build();
-        }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("^(?!auth).*$"))
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    private List<ApiKey> securitySchemes() {
+        return newArrayList(
+                new ApiKey("token", "token", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build()
+        );
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(
+                new SecurityReference("token", authorizationScopes));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API接口文档")
+                .build();
+    }
 }
