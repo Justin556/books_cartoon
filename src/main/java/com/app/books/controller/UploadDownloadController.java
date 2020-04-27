815c01c7291d1265077e12edff55d186888f990c
jacks <jackson20198899@gmail.com>
2020-04-27 20:55:42 +0400

    增加

--- src/main/java/com/app/books/controller/UploadDownloadController.java
+++ src/main/java/com/app/books/controller/UploadDownloadController.java
@@ -1,19 +1,20 @@
 package com.app.books.controller;
 
 
+import com.app.books.config.AuthenticationInterceptor;
+import com.app.books.mapper.UserMapper;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.UUID;
 
 /**
  * Created by jack on 2017/10/30.
@@ -21,9 +22,16 @@ import java.io.*;
 @RestController
 @RequestMapping("/uploadDownload")
 public class UploadDownloadController {
+
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Autowired
+    private UserMapper userMapper;
+
     private static final Logger logger = LoggerFactory.getLogger(UploadDownloadController.class);
 
-    private String uploadDir="C:\\Users\\jacks\\Pictures\\Saved Pictures";
+    private String uploadDir="/home/project/picture/touxiang";
 
     @RequestMapping(value = "/uploadImage", method = RequestMethod.POST)
     public JSONObject uploadImage(@RequestParam(value = "file") MultipartFile file) throws RuntimeException {
@@ -37,7 +45,7 @@ public class UploadDownloadController {
         // 文件上传后的路径
         String filePath = uploadDir;
         // 解决中文问题，liunx下中文路径，图片显示问题
-        // fileName = UUID.randomUUID() + suffixName;
+        fileName = UUID.randomUUID() + suffixName;
         File dest = new File(filePath + fileName);
         // 检测是否存在目录
         if (!dest.getParentFile().exists()) {
@@ -45,64 +53,13 @@ public class UploadDownloadController {
         }
         try {
             file.transferTo(dest);
+            userMapper.upload("/books/touxiang/"+fileName,Integer.parseInt(authenticationInterceptor.userId));
             logger.info("上传成功后的文件路径未：" + filePath + fileName);
 
         } catch (IllegalStateException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        return null;
-    }
-
-    //文件下载相关代码
-    @RequestMapping(value = "/downloadImage",method = RequestMethod.GET)
-    public String downloadImage(String imageName,HttpServletRequest request, HttpServletResponse response) {
-        //String fileName = "123.JPG";
-        logger.debug("the imageName is : "+imageName);
-        String fileUrl = uploadDir+imageName;
-        if (fileUrl != null) {
-            //当前是从该工程的WEB-INF//File//下获取文件(该目录可以在下面一行代码配置)然后下载到C:\\users\\downloads即本机的默认下载的目录
-           /* String realPath = request.getServletContext().getRealPath(
-                    "//WEB-INF//");*/
-            /*File file = new File(realPath, fileName);*/
-            File file = new File(fileUrl);
-            if (file.exists()) {
-                response.setContentType("application/force-download");// 设置强制下载不打开
-                response.addHeader("Content-Disposition",
-                        "attachment;fileName=" + imageName);// 设置文件名
-                byte[] buffer = new byte[1024];
-                FileInputStream fis = null;
-                BufferedInputStream bis = null;
-                try {
-                    fis = new FileInputStream(file);
-                    bis = new BufferedInputStream(fis);
-                    OutputStream os = response.getOutputStream();
-                    int i = bis.read(buffer);
-                    while (i != -1) {
-                        os.write(buffer, 0, i);
-                        i = bis.read(buffer);
-                    }
-                    System.out.println("success");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (fis != null) {
-                        try {
-                            fis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
         }
         return null;
     }
